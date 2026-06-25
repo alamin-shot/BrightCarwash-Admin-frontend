83c7c83066bfaa9396cae2bd8513c1bfc9685466
alamin-shot <rifat00175@gmail.com>
2026-06-25 11:43:41 +0600

    updated nextconfig.ts

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,14 @@
-// import type { NextConfig } from 'next';
-
-// const nextConfig: NextConfig = {
-// 	/* config options here */
-// 	allowedDevOrigins: ['10.10.33.10'],
-// 	images: {
-// 		unoptimized: true,
-// 	},
-// 	reactCompiler: true,
-// };
-
-// export default nextConfig;
-
-
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-	images: { unoptimized: true },
-	reactCompiler: true,
-	async rewrites() {
-		return [
-			{
-				source: "/api/:path*",
-				destination: "https://happens-sagem-wishes-basket.trycloudflare.com/api/:path*",
-			},
-		];
+	/* config options here */
+	allowedDevOrigins: ['10.10.33.10', "https://habits-obvious-advanced-textbook.trycloudflare.com"],
+	images: {
+		unoptimized: true,
 	},
+	reactCompiler: true,
 };
 
 export default nextConfig;
+
+

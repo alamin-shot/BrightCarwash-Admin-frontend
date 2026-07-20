2a90bb4965dacaa984592ca3bb7e11243daf4dea
alamin-shot <rifat00175@gmail.com>
2026-07-20 11:03:36 +0600

    updated next.config.ts file

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // experimental: { appDir: true }, // removed due to Next.js config change
+  transpilePackages: [
+    '@tiptap/react',
+    '@tiptap/extension-placeholder',
+    '@tiptap/starter-kit',
+    '@tiptap/extension-underline',
+    '@tiptap/extension-link',
+    '@tiptap/extension-image',
+    '@tiptap/extension-table',
+    '@tiptap/extension-table-cell',
+    '@tiptap/extension-table-header',
+    '@tiptap/extension-table-row',
+    '@tiptap/extension-text-align',
+    '@tiptap/extension-color',
+  ],
+};
+
+module.exports = nextConfig;

b318c5aa7509f1fc32e931d7c4d907c5469810b4
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2026-07-20 13:43:06 +0600

    Updated

--- src/components/shared/Notification.tsx
+++ src/components/shared/Notification.tsx
@@ -57,7 +57,7 @@ export default function Notification() {
       </button>
 
       <div
-        className={`absolute right-0 mt-2 w-72 lg:w-96 rounded-2xl border border-[#E5E7EB] bg-white p-4 shadow-[0px_0px_3px_rgba(0,0,0,0.12)] transition-all duration-200 ease-out ${
+        className={`absolute right-0 mt-2 sm:w-72 lg:w-96 rounded-2xl border border-[#E5E7EB] bg-white p-4 shadow-[0px_0px_3px_rgba(0,0,0,0.12)] transition-all duration-200 ease-out ${
           isOpen
             ? "visible translate-y-0 scale-100 opacity-100"
             : "invisible -translate-y-2 scale-95 opacity-0"
@@ -72,7 +72,7 @@ export default function Notification() {
                   className="flex items-center justify-between text-sm border-b border-[#E5E7EB] py-3"
                 >
                   <div className="space-y-2">
-                    <p className="text-sm lg:text-base font-semibold">
+                    <p className="text-sm lg:text-base font-semibold text-[#1D1F2C]">
                       {notification?.title}
                     </p>
                     <p className="text-[#777980] text-xs lg:text-sm line-clamp-1">
@@ -84,19 +84,19 @@ export default function Notification() {
                   </span>
                 </p>
               ))}
+              <Link
+                href="/notifications"
+                className="text-[#B23730] hover:text-[#B23730]/80 duration-300 text-sm font-semibold flex justify-center items-center gap-1 w-full cursor-pointer mt-4 "
+              >
+                View all <GoArrowRight />
+              </Link>
             </div>
           ) : (
-            <p className="text-center text-[#777980] text-xs lg:text-sm">
+            <p className="text-center flex items-center justify-center text-[#777980] text-xs lg:text-sm h-44">
               No notifications
             </p>
           )}
         </div>
-        <Link
-          href="/notifications"
-          className="text-[#B23730] hover:text-[#B23730]/80 duration-300 text-sm font-semibold flex justify-center items-center gap-1 w-full cursor-pointer mt-4 "
-        >
-          View all <GoArrowRight />
-        </Link>
       </div>
     </div>
   );
